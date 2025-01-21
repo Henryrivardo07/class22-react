4643499ecff7962ed11a34101596de6346a3fa97
henry rivardo <henryrivardo@outlook.com>
2025-01-21 18:53:51 +0700

    1

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,12 +6,14 @@ import { DataDisplay } from './3.customHook/DataDisplay/DataDisplay';
 import { MapState } from './2.stateWithMap/useMapAsState/MapState';
 import { CounterComponent } from './3.customHook/CounterDisplay/CounterDisplay';
 import { LocalStorageForm } from './3.customHook/LocalStorageForm/LocalStorageForm';
+import LemariDanAlarm from './contohUseEffect/LemariAndBook';
 
 export const Home: React.FC = () => {
   return (
     <div className={styles.home}>
       <h1 className={styles.title}>ReactJS Tutorial</h1>
       {/* <MapState/> */}
+      {/* <LemariDanAlarm/> */}
       <CounterComponent/>
       <LocalStorageForm />
       {/* <DataFetcher/> */}
